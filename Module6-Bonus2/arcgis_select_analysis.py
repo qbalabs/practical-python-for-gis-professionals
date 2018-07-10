e490b38a050aaba646715b0004756567469a8448
admin <admin@CORP.AMAZONWORKSPACES.COM>
2018-07-10 16:45:10 +0200

    Examples for Module6

---
+++ Module6-Bonus2/arcgis_select_analysis.py
@@ -0,0 +1,4 @@
+﻿# coding: utf-8
+import arcpy
+arcpy.env.workspace = "D:\\Users\\admin\\Desktop\\"
+arcpy.Select_analysis("ne\\ne.shp", "ne_selected_external.shp", 'NAME = \'Austin\'')
---
+++ Module6-Bonus2/arcigs_pdf_export.py
@@ -0,0 +1,12 @@
+﻿# coding: utf-8
+# Get the current project
+p=arcpy.mp.ArcGISProject('CURRENT')
+
+# And the first map from the project
+m=p.listMaps()[0]
+
+# And the main view that is ready for print
+m.defaultView
+
+# Then just export it to a PDF file
+m.defaultView.exportToPDF('D:\\Users\\admin\\Desktop\test.pdf', 800, 600)
